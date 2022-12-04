77af8ffbd76375ded24dd66426190a7a852f738e
John Pili <john@johnpili.com>
2022-12-04 21:07:22 +0800

    Initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module parse-json-data-without-struct
+
+go 1.19
---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+)
+
+var sampleJson = `{
+	"username": "johnpili",
+	"password": "L3akyPa5sw0rd!",
+	"name": "John Pili",
+	"website": "https://johnpili.com",
+	"number": 123
+}`
+
+func main() {
+	rawData := []byte(sampleJson)
+	var payload interface{}                  //The interface where we will save the converted JSON data.
+	err := json.Unmarshal(rawData, &payload) // Convert JSON data into interface{} type
+	if err != nil {
+		log.Fatal(err)
+	}
+	m := payload.(map[string]interface{}) // To use the converted data we will need to convert it into a map[string]interface{}
+
+	log.Printf("Username: %s", m["username"].(string))
+	log.Printf("Password: %s", m["password"].(string))
+	log.Printf("Name: %s", m["name"].(string))
+	log.Printf("Website: %s", m["website"].(string))
+	log.Printf("Number: %d", int(m["number"].(float64)))
+}
